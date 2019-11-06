cf58535230eb92a11688333a9f572d44380e680b
hrheydarian <hr.heidarian@gmail.com>
2019-11-06 14:40:30 +0100

    initial commit

---
+++ fish_project/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Documentation about Fish project"""
+import logging
+
+from .__version__ import __version__
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Hamidreza"
+__email__ = 'h.heydarian@tudelft.nl'
